f9c4664bccf7d3806df2418e0d715fb71c4e6321
hnino <hnino@wi-mobile.com>
2023-06-11 15:31:52 -0500

    Add feature 1

--- app/src/main/java/com/example/github/MainActivity.kt
+++ app/src/main/java/com/example/github/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.github
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,36 +15,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.github.ui.theme.GithubTheme
+import org.w3c.dom.Text
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            GithubTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.activity_main)
+        val btn_click_me = findViewById<Button>(R.id.btSaludar)
+        val etSaludar = findViewById<EditText>(R.id.etNombre)
+        val tvSaludar = findViewById<TextView>(R.id.textView)
+        btn_click_me.setOnClickListener {
+            tvSaludar.text = "Hello ${etSaludar.text}!"
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GithubTheme {
-        Greeting("Android CI/CD")
     }
 }
